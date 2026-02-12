fe4082c32fd851b6f27f403c23127116058dfc94
Luan-Galera <luangalera17@gmail.com>
2026-02-12 10:07:40 -0300

    feat: configurando servidor Vite para desenvolvimento

--- 02-react-vite/vite.config.js
+++ 02-react-vite/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    port: 3000, //Muda a porta padrão vite e react
+    open: true, //Abre o navegador automaticamente
+    host: true, //Permite que tenha acesso via IP na rede local
+  },
 })
