446dfabca6462b1dff7c27890c1ade709a9b1e82
Luan-Galera <luangalera17@gmail.com>
2026-02-12 16:56:27 -0300

    feat: adicionar componentes de Link, Rodape e SocialLink, e atualizar o componente Perfil

--- 03-devlinks/src/App.jsx
+++ 03-devlinks/src/App.jsx
@@ -1,18 +1,32 @@
 import './App.css'
+import Link from './components/Link/Link'
 import Perfil from './components/Perfil/perfil'
+import Rodape from './components/Rodape/Rodape'
+import SocialLink from './components/SocialLink/SocialLink'
 
 function App() {
 
   return (
     <div id='App'>
-      <Perfil />
+      <Perfil fotoPerfil={"https://placehold.co/100"}>NotFound</Perfil>
 
       <div className='switch'>
         botão switch
       </div>
-      <div className='links'></div>
-      <div className='socialLinks'></div>
-      <div className='rodape'></div>
+      <ul>
+        <Link url={""}>Inscreva-se</Link>
+        <Link url={""}>Minha Playlist</Link>
+        <Link url={""}>Me pague um café!</Link>
+        <Link url={""}>Conheça o Curso DEV</Link>
+      </ul>
+
+      <div className='socialLinks'>
+        <SocialLink url={"https://github.com/"} icon={"logo-github"} />
+        <SocialLink url={"https://www.instagram.com/"} icon={"logo-instagram"} />
+        <SocialLink url={"https://www.youtube.com/"} icon={"logo-youtube"} />
+        <SocialLink url={"https://br.linkedin.com/"} icon={"logo-linkedin"} />
+      </div>
+      <Rodape>Luan-Galera</Rodape>
     </div>
   )
 }
---
+++ 03-devlinks/src/components/SocialLink/SocialLink.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SocialLink = ({url, icon}) => {
+  return (
+    <div>
+      <a href={url}>
+        <ion-icon name={icon}></ion-icon>
+      </a>
+    </div>
+  )
+}
+
+export default SocialLink
