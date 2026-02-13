fec705c6fa15d517c19df186a3e80551fd4d8ba3
Luan-Galera <luangalera17@gmail.com>
2026-02-13 10:41:53 -0300

    feat: adicionar id ao componente Link e organizar a estrutura do HTML

--- 03-devlinks/src/App.jsx
+++ 03-devlinks/src/App.jsx
@@ -13,12 +13,15 @@ function App() {
       <div className='switch'>
         botão switch
       </div>
-      <ul>
-        <Link url={""}>Inscreva-se</Link>
-        <Link url={""}>Minha Playlist</Link>
-        <Link url={""}>Me pague um café!</Link>
-        <Link url={""}>Conheça o Curso DEV</Link>
-      </ul>
+      
+      <div id="Link">
+        <ul>
+          <Link url={""}>Inscreva-se</Link>
+          <Link url={""}>Minha Playlist</Link>
+          <Link url={""}>Me pague um café!</Link>
+          <Link url={""}>Conheça o Curso DEV</Link>
+        </ul>
+      </div>
 
       <div className='socialLinks'>
         <SocialLink url={"https://github.com/"} icon={"logo-github"} />
