93d5768b9f274ca86dcca4487391bbbacf11f329
Luan-Galera <luangalera17@gmail.com>
2026-02-13 08:13:41 -0300

    feat: remover elemento div desnecessário do componente SocialLink

--- 03-devlinks/src/components/SocialLink/SocialLink.jsx
+++ 03-devlinks/src/components/SocialLink/SocialLink.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 
 const SocialLink = ({url, icon}) => {
   return (
-    <div>
       <a href={url}>
         <ion-icon name={icon}></ion-icon>
       </a>
-    </div>
   )
 }
 
