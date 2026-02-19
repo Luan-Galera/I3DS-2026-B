b8a0fd3eec605e1fc2373636f9b04c53104043b1
Luan-Galera <luangalera17@gmail.com>
2026-02-19 11:26:09 -0300

    em App.jsx: - Alterando import do App.css - Fazendo import das imagens logo, lupa - Fazendo import do Rodape - Inserindo Logo, e parte para pesquisa - Inserindo Rodape ao final da página
    No projeto:
    - Criando pasta components
    - Adicionando o componente Rodape

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,11 +1,26 @@
-import React from './App.css'
+import "./App.css";
+
+import logo from "./assets/devflix.png";
+import lupa from "./assets/search.svg";
+import Rodape from "./components/Rodape/Rodape";
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div id="App">
+      <img
+        id="logo"
+        src={logo}
+        alt="Logotipo do serviço de streaming DEVFLIX em fundo preto com letras vermelhas, destaque na plataforma de entretenimento digital."
+      />
 
-export default App
+      <div className="search">
+        <input type="text" placeholder="Pesquise por filmes e séries..." />
+        <img src={lupa} alt="Botão pesquisar!" />
+      </div>
+
+      <Rodape>Luan-Galera</Rodape>
+    </div>
+  );
+};
+
+export default App;
